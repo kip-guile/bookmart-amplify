4e1786c8d0e5454bf3eb869b2348f6439fca1866
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-10-04 08:05:48 +0100

    authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import { Authenticator, AmplifyTheme } from 'aws-amplify-react'
+import useAmplifyAuth from './components/UseAmplifyAuth'
+import './App.css'
 
-class App extends React.Component {
-  state = {};
+function App() {
+  const {
+    state: { user },
+    handleSignOut,
+  } = useAmplifyAuth()
 
-  render() {
-    return <div>App</div>;
-  }
+  console.log('user =>', user)
+  console.dir(AmplifyTheme)
+
+  return !user ? <Authenticator theme={theme} /> : <div>App</div>
 }
 
-export default App;
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme.navBar,
+    backgroundColor: '#7700b4',
+    color: 'white',
+  },
+  button: {
+    ...AmplifyTheme.button,
+    backgroundColor: '#177bff',
+    color: 'white',
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: '5px',
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: '#7700b4',
+  },
+}
+
+export default App
+
+// export default withAuthenticator(App, true, [], null, theme)
